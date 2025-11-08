6c9e270fb7d16bd1fd0851e0eba49f70349afb25
Katsuu <50086027+Katsuuii@users.noreply.github.com>
2025-11-08 10:29:35 +0800

    Delete demo/demo directory

--- demo/demo/src/main/java/OrderLineDAO.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-
-public interface OrderLineDAO {
-    void insert(OrderLineController orderline);
-    List<OrderLineController> findAll();
-    void delete(String id);
-}
