40077a45c4368b2b486e8db19a95592816b0cfdb
Mark Baja <50086027+Katsuuii@users.noreply.github.com>
2026-01-07 11:13:20 +0800

    Refactor Removal of extension to make Main.

--- src/main/java/Controller/AlleviationController.java
+++ src/main/java/Controller/AlleviationController.java
@@ -1,6 +1,5 @@
 package Controller;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,27 +12,23 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AlleviationController {
+
     private Stage stage;
     private Scene scene;
     private Parent root;
-    public static class Main extends Application {
-        @Override
-        public void start(Stage stage) {
-            try {
-                Parent root = FXMLLoader.load(
-                        getClass().getResource("/FXML/Alleviation.fxml")
-                );
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-                stage.show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+
     @FXML
-    public void MainMenuLog(ActionEvent event)throws IOException {
+    public void MainMenuLog(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/FXML/login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void ToCEO(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/FXML/CEO.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
@@ -48,15 +43,10 @@ public class AlleviationController {
         stage.setScene(scene);
         stage.show();
     }
-
     @FXML
     private Button CloseButton;
-    @FXML
-    protected void onCloseButtononAction(ActionEvent event) {
+    public void onCloseButtononAction(ActionEvent actionEvent) {
         Stage stage = (Stage) CloseButton.getScene().getWindow();
         stage.close();
     }
-
-
 }
-
