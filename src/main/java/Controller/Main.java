ab37f619bd89130b93a1ca060f1edcf42bae586c
Katsuuii <kamote2006@gmail.com>
2026-01-28 23:11:36 +0800

    Current working project state

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -5,7 +5,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
         var url = getClass().getResource("/FXML/Alleviation.fxml");
